5610b7d795f26324dcea6b9341d76712ce2312a0
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-03-06 20:43:46 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
